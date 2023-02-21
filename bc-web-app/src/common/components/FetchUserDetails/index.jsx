a5bde32767d93f5583167047309a29c5d3d74a72
Kevon Mayers <novekm@amazon.com>
2023-02-21 01:12:45 -0500

    Change console log for FetchUserDetails component

--- bc-web-app/src/common/components/FetchUserDetails/index.jsx
+++ bc-web-app/src/common/components/FetchUserDetails/index.jsx
@@ -8,7 +8,7 @@ const FetchUserDetails = ({ user }) => {
   // This function uses the prop 'user' that is passed from the parent 'App' component
   // The function takes this prop and passes the variable as a prop to all components listed in the return., assigns them to the variable 'amplifySignedInUser'
 
-  console.log(`I am the child of App component`);
+  console.log(`Fetch User Details Component:`);
   console.log(user);
 
   return <TopNavigationHeader user={user} />;
